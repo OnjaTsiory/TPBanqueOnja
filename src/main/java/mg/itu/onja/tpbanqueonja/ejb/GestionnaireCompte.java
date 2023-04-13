6bdfdb87d61eaf13b447d600ba1a09a5b2be4c8c
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-04-13 08:35:36 +0300

    Création d'un EJB session stateless GestionnaireCompte

---
+++ src/main/java/mg/itu/onja/tpbanqueonja/ejb/GestionnaireCompte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.onja.tpbanqueonja.ejb;
+
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author ELITEBOOK
+ */
+@Stateless
+public class GestionnaireCompte {
+    
+}
