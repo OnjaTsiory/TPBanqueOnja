c81ea6ef5a741b12a357ebc2f43f03955b28ab3d
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-04-13 08:48:25 +0300

    Déclaration de la source de données

--- src/main/java/mg/itu/onja/tpbanqueonja/ejb/GestionnaireCompte.java
+++ src/main/java/mg/itu/onja/tpbanqueonja/ejb/GestionnaireCompte.java
@@ -4,12 +4,26 @@
  */
 package mg.itu.onja.tpbanqueonja.ejb;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
 
 /**
  *
  * @author ELITEBOOK
  */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="otuser",              // nom et
+    password="#mdpMysql123", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
 @Stateless
 public class GestionnaireCompte {
     
